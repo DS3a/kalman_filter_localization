18eb390c83e621a69a506a9079d777c4b75875bc
DS3a <33351122+DS3a@users.noreply.github.com>
2022-11-11 02:01:38 +0530

    initial commit

--- include/kalman_filter.hpp
+++ include/kalman_filter.hpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <eigen3/Eigen/Core>
+#include <iostream>
 
 namespace KalmanFilter {
   class LinearKalmanFilter {
@@ -7,34 +8,104 @@ namespace KalmanFilter {
     // imu input for acceleration (needs to be filtered to remove gravity using orientation)
     // gps to give position
     public:
-    KalmanFilter() {
+    LinearKalmanFilter(double input_variance) {
+      input_variance = input_variance;
+      // input is velocity from encoders
+
+      state.setZero();
+      previous_state = state;
+
+      state_covariance.setIdentity();
+      previous_state_covariance = state_covariance;
     }
 
     void predict(double dt, double control_signal, double cov) {
       // dt = time delay
       // control signal is the velocity we are getting from the wheel encoder
       // cov = process noise covariance
+
+      A << 1, dt, 0.5*dt*dt, 0, 0, dt, 0, 0, 1;
+      // A is the transition matrix
+
+      U << 0, control_signal, 0;
+      // U is the input vector
+
+      // Q is the process noise covariance
+
+      predicted_state = (A * previous_state) + U;
+      std::cout << predicted_state << std::endl;
+
+      // debugging steps
+      state = predicted_state;
+      previous_state = state;
     }
 
     void measure_acceleration(double dt, double acceleration, double cov) {
       // function to update the state belief based on acceleration data
       // dt = time delay
       // acceleration = acceleration measurement
-      // cov = acceleration covariance
+      // cov = acceleration measurement covariance
+    }
+
+    void measure_velocity(double dt, double velocity, double cov) {
+      // function to update the state of the belief based on velocity measurement
+      // dt = time delay
+      // velocity = velocity measurement
+      // cov = velocity measurement covariance
     }
 
     void measure_position(double dt, double position, double cov) {
       // function to update state belief based on position data
       // dt = time
       // position = position measurement
-      // cov = position covariance
+      // cov = position measurement covariance
     }
 
     void update() {
+      // function to update the current state
+      previous_state = state;
+
+      // modify state based on sensor measurements
     }
 
+    void modify_input_variance(double variance) {
+      input_variance = variance;
+    }
+
+    double get_position() const {
+      return state[0];
+    }
+
+    double get_velocity() const {
+      return state[1];
+    }
+
+    double get_acceleration() const {
+      return state[2];
+    }
 
     private:
-    Eigen::Matrix<double, > state;
+    Eigen::Matrix<double, 3, 3> A;
+    // A is the transition matrix
+
+    Eigen::Matrix<double, 3, 1> U;
+    // U is the input matrix input is the velocity from encoders
+
+    Eigen::Matrix<double, 3, 3> Q;
+    // Q is the process noise covariance matrix
+
+    Eigen::Matrix<double, 3, 1> previous_state;
+    Eigen::Matrix<double, 3, 1> predicted_state;
+    Eigen::Matrix<double, 3, 1> state;
+    // state consists of x, x_dot, and x_dot_dot
+    // i.e. position, velocity, and acceleration in one dimension
+
+    Eigen::Matrix<double, 3, 3> previous_state_covariance;
+    Eigen::Matrix<double, 3, 3> predicted_state_covariance;
+    Eigen::Matrix<double, 3, 3> state_covariance;
+    // 3x3 matrix containing the state covariance
+
+    double input_variance;
+    // velocity from encoders
   };
 }
--- src/localization.cpp
+++ src/localization.cpp
@@ -6,7 +6,9 @@ int main(int argc, char ** argv)
   (void) argc;
   (void) argv;
 
-  KalmanFilter::KalmanFilter kf;
-  printf("hello world kalman_filter_localization package\n");
+  KalmanFilter::LinearKalmanFilter lkf(0.01);
+  printf("current position: %f\n\n", lkf.get_position());
+  for(int i=0; i<11; i++)
+    lkf.predict(0.01, 0.2, 0.01);
   return 0;
 }
